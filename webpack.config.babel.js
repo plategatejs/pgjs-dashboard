6d438a1ab5ed4fe2836267236562f8656eed0130
kazet <k@zet.email>
2016-01-07 21:26:33 +0100

    update webpack config

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,17 +1,46 @@
 'use strict';
 
+import webpack from 'webpack';
+import config from 'config';
+
+const isDev = !!~process.argv.indexOf('-d');
+
+const host = config.get('api');
+const prefix = '/api';
+const proxy = {
+  [`${prefix}/*`]: {
+    target: host,
+    secure: false,
+    bypass: req => {
+      req.url = req.url.substr(prefix.length, req.url.length);
+      return false;
+    }
+  }
+};
+
 export default {
   entry: [
-    './index.js'
+    './src/index.js',
+    'file?name=index.html!./src/index.html'
   ],
   output: {
-    filename: 'bundle.js'
+    path: './dist',
+    filename: 'dashboard.js',
+    sourceMapFilename: 'dashboard.js.map'
   },
   module: {
     loaders: [{
       test: /\.js$/,
-      loader: 'babel-loader',
+      loader: 'babel',
       exclude: /node_modules/
     }]
-  }
+  },
+  devServer: { proxy },
+  plugins: isDev ? [] : [
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      compress: { warnings: false },
+      output: { comments: false }
+    })
+  ]
 };
